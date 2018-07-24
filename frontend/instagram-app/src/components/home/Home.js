8809b9ada1057cba8b42a19afe2393e186ec7ce6
caroline-cdmx <caroline.fernandesmartins@outlook.fr>
2018-07-23 21:52:07 -0500

    modify navbar

--- frontend/instagram-app/src/components/home/Home.js
+++ frontend/instagram-app/src/components/home/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Clone Instagram</h1>
+        <h1></h1>
       </div>
     )
   }
--- frontend/instagram-app/src/components/nav/Nav.js
+++ frontend/instagram-app/src/components/nav/Nav.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import './style.css';
 
 const Nav = () => (
-  <nav class="navbar navbar-expand-lg fixed-top">
-    <i class="fab fa-instagram fa-2x"></i>
-    <a class="navbar-brand" href="#">Instagram</a>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search"/>
+  <nav className="navbar navbar-expand-lg fixed-top">
+    <a className="brand-name">
+      <i className="fab fa-instagram fa-2x"></i>
+    </a>
+    <a className="navbar-brand" href="#">Instagram</a>
+    <form className="form-inline">
+      <input className="form-control mr-sm-2 mx-auto" type="search" placeholder="Buscar" aria-label="Search"/>
     </form>
-    <div class="collapse navbar-collapse float-right" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Regístrate</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Regístrate</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Iniciar sesión</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Iniciar sesión</a>
         </li>
       </ul>
     </div>
